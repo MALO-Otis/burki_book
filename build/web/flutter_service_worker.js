c032d9b92c851dd31ca2cd9d4377a6c71109dd65
malootis <malootis52@gmail.com>
2025-07-29 12:03:30 +0200

    build(web): release build for deployment

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -29,7 +29,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "a112d08e1aa4c58f724e2da374a96b18
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "ebd7da102c2f0f42507c32bee6c1206d",
+"flutter_bootstrap.js": "bc719f4b6563a31f6883a4835e627477",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
